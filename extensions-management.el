cdf58add01a083ae75e6510fa2208fdd3eb19b1d
Aliaksiej Artamonaŭ <aliaksiej.artamonau@gmail.com>
2010-08-15 19:34:25 +0300

    Extensions management:
     - absolute paths handling

--- extensions-management.el
+++ extensions-management.el
@@ -2,13 +2,26 @@
 
 (setq load-path
       (append load-path
-	      `(,extensions-dir "~/emacs/rc")))
+              `(,extensions-dir "~/emacs/rc")))
+
+
+(defun extension-path (path)
+  "If supplied path is absolute then it's returned without changes. It's treated
+as a file in extension directory."
+  (if (file-name-absolute-p path)
+      path
+    (concat extensions-dir "/" name)))
+
 
 (defun  add-extension-load-path (name)
+  "Adds extension to the load-path."
   (setq load-path
-	(append load-path
-		(list (concat extensions-dir "/" name)))))
+        (append load-path
+                (list (extension-path name)))))
+
+
 (defun add-extension-exec-path (name)
+  "Adds extension to the exec-path."
   (setq exec-path
-	(append exec-path
-		(list (concat extensions-dir "/" name)))))
+        (append exec-path
+                (list (extension-path name)))))
