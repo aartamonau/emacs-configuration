2b6157e5c5a1c8d8c3b115b3854c6b53de7321d9
Aliaksiej Artamonaŭ <aliaksiej.artamonau@gmail.com>
2010-08-19 20:44:00 +0300

    haskell-mode:   - cleanup

--- rc/emacs-rc-haskell.el
+++ rc/emacs-rc-haskell.el
@@ -1,16 +1,4 @@
-(add-extension-exec-path "haskell")
-
-(setq auto-mode-alist
-      (append auto-mode-alist
-              '(("\\.[hg]s$"  . haskell-mode)
-                ("\\.hi$"     . haskell-mode)
-                ("\\.l[hg]s$" . literate-haskell-mode))))
-
-(autoload 'haskell-mode "haskell-mode"
-  "Major mode for editing Haskell scripts." t)
-
-(autoload 'literate-haskell-mode "haskell-mode"
-  "Major mode for editing literate Haskell scripts." t)
+(load "haskell-site-file")
 
 (add-hook 'haskell-mode-hook 'global-hook-handler)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
@@ -28,32 +16,3 @@
   (autoload 'haskell-refac-mode "haskell-refac"
     "Minor mode for refactoring Haskell programs" t)
   (add-hook 'haskell-mode-hook 'haskell-refac-mode))
-
-
-;; (load "emacs-rc-pretty-lambda.el")
-
-;; (defun haskell-unicode ()
-;;   (interactive)
-;;   (substitute-patterns-with-unicode
-;;    (list (cons "\\(<-\\)" 'left-arrow)
-;;       (cons "\\(->\\)" 'right-arrow)
-;;       (cons "\\(==\\)" 'identical)
-;;       (cons "\\(/=\\)" 'not-identical)
-;;       (cons "\\(()\\)" 'nil)
-;;       (cons "\\<\\(sqrt\\)\\>" 'square-root)
-;;       (cons "\\(&&\\)" 'logical-and)
-;;       (cons "\\(||\\)" 'logical-or)
-;;       (cons "\\<\\(not\\)\\>" 'logical-neg)
-;;       (cons "\\(>\\)\\[^=\\]" 'greater-than)
-;;       (cons "\\(<\\)\\[^=\\]" 'less-than)
-;;       (cons "\\(>=\\)" 'greater-than-or-equal-to)
-;;       (cons "\\(<=\\)" 'less-than-or-equal-to)
-;;       (cons "\\<\\(alpha\\)\\>" 'alpha)
-;;       (cons "\\<\\(beta\\)\\>" 'beta)
-;;       (cons "\\<\\(gamma\\)\\>" 'gamma)
-;;       (cons "\\<\\(delta\\)\\>" 'delta)
-;;       (cons "\\(''\\)" 'double-prime)
-;;       (cons "\\('\\)" 'prime)
-;;       (cons "\\(!!\\)" 'double-exclamation)
-;;       (cons "\\(\\.\\.\\)" 'horizontal-ellipsis))))
-;; (add-hook 'haskell-mode-hook 'haskell-unicode)
