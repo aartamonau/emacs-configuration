98e183902d00a3827f9e57690fdcb7ab5251774c
Aleksey Artamonov <AlexeyArt@synesis.ru>
2010-09-16 16:06:09 +0300

    Linum-mode is not activated in the buffers having doc-view-mode major mode.

--- rc/emacs-rc-linum.el
+++ rc/emacs-rc-linum.el
@@ -1,3 +1,12 @@
 ;; emacs-rc-linum.el
 
 (global-linum-mode 1)
+
+(defconst linum-mode-excludes '(doc-view-mode)
+  "List of major modes preventing linum to be enabled in the buffer.")
+
+(defadvice linum-mode (around linum-mode-selective activate)
+  "Avoids enabling of linum-mode in the buffer having major mode set to one
+of listed in `linum-mode-excludes'."
+  (unless (member major-mode linum-mode-excludes)
+    ad-do-it))
