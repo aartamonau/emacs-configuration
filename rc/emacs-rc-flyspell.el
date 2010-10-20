42a50dada5dae07b694704b82024d79f180e50b2
Aleksey Artamonov <AlexeyArt@synesis.ru>
2010-10-20 16:30:37 +0300

    Flyspell mode configuration adjustments:   1. Using custom-set-variables instead of `setq'.   2. Common dictionary program's options moved to `ispell-extra-args'.   3. `-a' option removed from `ispell-extra-args' as the mode supplies it to      the dictionary program anyway (such duplication caused errors      'ispell-init-process' on Ubuntu 10.04).

--- rc/emacs-rc-flyspell.el
+++ rc/emacs-rc-flyspell.el
@@ -1,22 +1,12 @@
-(eval-after-load "ispell"
-  '(progn
-     (setq ispell-program-name "hunspell"
-           ispell-dictionary "american"
-           ispell-extra-args '("-a" "-i" "utf-8")
-           ispell-silently-savep t
-           ispell-local-dictionary-alist
-           '((nil ; default
-              "[A-Za-z]" "[^A-Za-z]" "[']" t ("-d" "en_US" "-i" "utf-8") nil utf-8)
-             ("american" ; US English
-              "[A-Za-z]" "[^A-Za-z]" "[']" t ("-d" "en_US" "-i" "utf-8") nil utf-8)
-             ("russian" ; russian
-              "[А-Яа-я]" "[^А-Яа-я]" "[']" t ("-d" "ru_RU" "-i" "utf-8") nil utf-8)
-             )
-           )))
-
+(custom-set-variables
+ '(ispell-program-name "hunspell")
+ '(ispell-dictionary "american")
+ '(ispell-extra-args '("-i" "utf-8"))
+ '(ispell-silently-savep t)
+ '(ispell-local-dictionary-alist
+   '(("american"
+      "[A-Za-z]" "[^A-Za-z]" "[']" t ("-d" "en_US") nil utf-8)
+     ("russian"
+      "[А-Яа-я]" "[^А-Яа-я]" "[']" t ("-d" "ru_RU") nil utf-8))))
 
 (add-hook 'text-mode-hook 'turn-on-flyspell)
-;; (add-hook 'flyspell-mode-hook
-;;           (lambda ()
-;;             (set-process-query-on-exit-flag
-;;               (get-buffer-process (current-buffer)) nil)))
