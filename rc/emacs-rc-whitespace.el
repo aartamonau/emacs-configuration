d3d411e1a3b9d0de89c26e2dc55fd5c6c7927237
Aliaksiej Artamonaŭ <aliaksiej.artamonau@gmail.com>
2011-03-26 20:07:06 +0200

    Make whitespace-mode use faces (fixes highlighting on newer versions of emacs).

--- rc/emacs-rc-whitespace.el
+++ rc/emacs-rc-whitespace.el
@@ -2,4 +2,4 @@
 
 (custom-set-variables
  '(whitespace-global-modes '(not org-mode))
- '(whitespace-style  '(tabs trailing lines-tail empty tab-mark)))
+ '(whitespace-style '(face tabs trailing lines-tail empty tab-mark)))
