8421dcb2e70e1c19ee0d252fbccad7899182cd52
Aliaksiej Artamonaŭ <aliaksiej.artamonau@gmail.com>
2011-03-26 20:19:23 +0200

    Remove any mentions of HaRe.

--- rc/emacs-rc-haskell.el
+++ rc/emacs-rc-haskell.el
@@ -6,13 +6,3 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 (add-hook 'haskell-mode-hook #'(lambda () (require 'inf-haskell)))
-
-
-(defconst HaRe-path
-  (car (directory-files "/usr/share/" t "HaRe.*")))
-
-(when HaRe-path
-  (add-extension-load-path HaRe-path)
-  (autoload 'haskell-refac-mode "haskell-refac"
-    "Minor mode for refactoring Haskell programs" t)
-  (add-hook 'haskell-mode-hook 'haskell-refac-mode))
