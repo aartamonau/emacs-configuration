d004df24684102645c80bfb89be6286ccaf0e232
Aliaksiej Artamonaŭ <aliaksiej.artamonau@gmail.com>
2011-04-22 13:57:52 +0300

    view-mode: basic Vim-like key mappings.

--- rc/emacs-rc-view-mode.el
+++ rc/emacs-rc-view-mode.el
@@ -2,3 +2,16 @@
   "Activates view-mode in buffer if it is made read-only"
   (let ((toggle (if buffer-read-only 1 0)))
     (view-mode toggle)))
+
+(defun my/view-mode-hook ()
+  (define-key view-mode-map (kbd "j") 'next-line)
+  (define-key view-mode-map (kbd "k") 'previous-line)
+  (define-key view-mode-map (kbd "h") 'backward-char)
+  (define-key view-mode-map (kbd "l") 'forward-char)
+
+  (define-key view-mode-map (kbd "G")
+    (lambda ()
+      (interactive)
+      (View-goto-percent 100))))
+
+(add-hook 'view-mode-hook 'my/view-mode-hook)
