c4043e15d8022b27515bd701bd8073bd160a52e1
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2012-04-14 16:18:50 +0300

    Use vim-like navigation keys in magit log.

--- rc/emacs-rc-magit.el
+++ rc/emacs-rc-magit.el
@@ -1,3 +1,6 @@
 (require 'magit)
 
 (global-set-key (kbd "C-x g") 'magit-status)
+
+(define-key magit-log-mode-map (kbd "j") 'magit-goto-next-section)
+(define-key magit-log-mode-map (kbd "k") 'magit-goto-previous-section)
