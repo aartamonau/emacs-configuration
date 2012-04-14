8c62eb504905c83f04de7e9677f368d7b9b5040a
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2012-04-14 16:23:29 +0300

    C-x o switches to the next window *clockwise*.

--- rc/emacs-rc-switch-window.el
+++ rc/emacs-rc-switch-window.el
@@ -16,9 +16,9 @@
 (global-set-key (kbd "C-x o")
                 (lambda (force-switch-mode)
                   (interactive "P")
-                  (aa/switch-window -1 force-switch-mode)))
+                  (aa/switch-window 1 force-switch-mode)))
 
 (global-set-key (kbd "C-x O")
                 (lambda (force-switch-mode)
                   (interactive "P")
-                  (aa/switch-window 1 force-switch-mode)))
+                  (aa/switch-window -1 force-switch-mode)))
