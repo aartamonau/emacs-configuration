9a3cf5eab0f9488ab66691a98a7381600dbddc86
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2012-04-15 23:51:43 +0300

    Make vc-annotate ignore whitespace.

--- rc/emacs-rc-magit.el
+++ rc/emacs-rc-magit.el
@@ -4,3 +4,8 @@
 
 (define-key magit-log-mode-map (kbd "j") 'magit-goto-next-section)
 (define-key magit-log-mode-map (kbd "k") 'magit-goto-previous-section)
+
+;; ignore whitespace in git blame
+(defun vc-git-annotate-command (file buf &optional rev)
+  (let ((name (file-relative-name file)))
+    (vc-git-command buf 0 name "blame" "-w" rev)))
