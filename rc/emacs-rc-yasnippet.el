994f8b619015207620b81e608f05e7b1a08253ad
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2012-07-15 01:51:57 -0700

    Stupid snippet for creating two-sided english org-drill cards.

--- rc/emacs-rc-yasnippet.el
+++ rc/emacs-rc-yasnippet.el
@@ -6,3 +6,16 @@
 (setq yas/prompt-functions '(yas/ido-prompt))
 
 (yas/load-directory yas/root-directory)
+
+(defun yas/org-very-safe-expand ()
+  (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (make-variable-buffer-local 'yas/trigger-key)
+            (setq yas/trigger-key [tab])
+            (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
+            (define-key yas/keymap [tab] 'yas/next-field)
+            (define-key yas/keymap [S-iso-lefttab] 'yas/prev-field)
+            (define-key yas/keymap [(shift tab)] 'yas/prev-field)
+            (define-key yas/keymap [backtab] 'yas/prev-field)))
