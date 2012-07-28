9962cc5687255b884c2bf2a2f77a427da91fbd70
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2012-07-28 04:52:28 +0300

    Kill emacs daemon only when there're no other clients.

--- rc/emacs-rc-daemon.el
+++ rc/emacs-rc-daemon.el
@@ -3,17 +3,10 @@
     "Kill emacs server with the last client. Otherwise just close the client"
     (interactive)
     ;; when only one client left just kill the server
-    (if (= (length server-clients) 1)
-        (progn
-          ;; remove `server-kill-emacs-query-function' from
-          ;; `kill-emacs-query-functions' to avoid a message about active
-          ;; clients (we're the only client)
-          (setq kill-emacs-query-functions
-                (remq 'server-kill-emacs-query-function
-                      kill-emacs-query-functions))
-                (save-buffers-kill-emacs))
-          ;; otherwise just kill current client
-          (save-buffers-kill-terminal)))
+    (if (= (length server-clients) 0)
+        (save-buffers-kill-emacs)
+      ;; otherwise just kill current client
+      (save-buffers-kill-terminal)))
 
   (define-key global-map (kbd "C-x C-c")
     'my/daemon-maybe-save-buffers-kill-emacs))
