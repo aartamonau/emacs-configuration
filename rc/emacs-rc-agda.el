1f5621e11dd1cf24a4036f29e3753b6bcf2c709d
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-08-31 13:29:04 -0700

    Better agda-mode colors for dark background.

--- rc/emacs-rc-agda.el
+++ rc/emacs-rc-agda.el
@@ -2,3 +2,18 @@
   (setq agda2-include-dirs `("." "~/Agda/lib/" "/usr/lib/agda"))
   (load-file (let ((coding-system-for-read 'utf-8))
                (shell-command-to-string "agda-mode locate"))))
+
+;; taken from https://lists.chalmers.se/pipermail/agda/2012/004597.html
+(custom-set-faces
+ '(agda2-highlight-datatype-face ((t (:inherit font-lock-type-face))))
+ '(agda2-highlight-field-face ((t (:foreground "#ad7fa8"))))
+ '(agda2-highlight-function-face ((t (:inherit font-lock-function-name-face))))
+ '(agda2-highlight-inductive-constructor-face ((t (:foreground "#8ae234"))))
+ '(agda2-highlight-keyword-face ((t (:inherit font-lock-keyword-face))))
+ '(agda2-highlight-module-face ((t (:inherit font-lock-builtin-face))))
+ '(agda2-highlight-number-face ((t (:inherit font-lock-constant-face))))
+ '(agda2-highlight-postulate-face ((t (:inherit font-lock-type-face))))
+ '(agda2-highlight-primitive-face ((t (:inherit font-lock-type-face))))
+ '(agda2-highlight-primitive-type-face ((t (:inherit font-lock-type-face))))
+ '(agda2-highlight-record-face ((t (:inherit font-lock-type-face))))
+ '(agda2-highlight-string-face ((t (:inherit font-lock-string-face)))))
