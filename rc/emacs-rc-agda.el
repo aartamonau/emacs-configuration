29f7c34efa9300b1a1cff73d1885efab0c39b7dd
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-08-31 13:18:32 -0700

    Add more include paths to agda.

--- rc/emacs-rc-agda.el
+++ rc/emacs-rc-agda.el
@@ -1,4 +1,4 @@
 (when (executable-find "agda-mode")
-  (setq agda2-include-dirs `("~/Agda/lib/"))
+  (setq agda2-include-dirs `("." "~/Agda/lib/" "/usr/lib/agda"))
   (load-file (let ((coding-system-for-read 'utf-8))
                (shell-command-to-string "agda-mode locate"))))
