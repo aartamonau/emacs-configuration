9aa52eb42a62cb709bcb8bc391e68543d759cde3
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-09-26 16:31:45 -0700

    Default indentation for haskell.

--- rc/emacs-rc-haskell.el
+++ rc/emacs-rc-haskell.el
@@ -3,8 +3,6 @@
 (add-hook 'haskell-mode-hook 'global-hook-handler)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 (add-hook 'haskell-mode-hook #'(lambda () (require 'inf-haskell)))
 
 (autoload 'ghc-init "ghc" nil t)
