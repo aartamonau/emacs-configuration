359214838ae6d53e94446ad65697a84b8075dc57
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-12-05 13:46:37 -0800

    Use custom godef-jump allowing to open result in other window.

--- rc/emacs-rc-go-mode.el
+++ rc/emacs-rc-go-mode.el
@@ -1,4 +1,8 @@
 (add-hook 'go-mode-hook
           (lambda () (c-subword-mode)))
 (add-hook 'go-mode-hook (lambda ()
-                          (local-set-key (kbd "M-.") 'godef-jump)))
+                          (local-set-key (kbd "M-.") 'my/godef-jump)))
+
+(defun my/godef-jump (point other-window)
+  (interactive "d\nP")
+  (godef-jump point other-window))
