62ecba4f614c0ae1e9941fa374f0ea82b4cff9a7
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-12-19 13:04:19 -0800

    Basic mu4e setup.

---
+++ rc/emacs-rc-mu4e.el
@@ -0,0 +1,31 @@
+(require 'mu4e)
+
+(setq user-mail-address "aliaksiej.artamonau@gmail.com")
+(setq user-full-name "Aliaksey Artamonau")
+
+(setq mu4e-maildir "~/mail/aliaksiej.artamonau@gmail.com")
+(setq mu4e-drafts-folder "/[Gmail].Drafts")
+(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+(setq mu4e-trash-folder  "/[Gmail].Trash")
+
+;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+(setq mu4e-sent-messages-behavior 'delete)
+
+(setq mu4e-maildir-shortcuts
+      '( ("/INBOX"               . ?i)
+         ("/[Gmail].Sent Mail"   . ?s)
+         ("/[Gmail].Trash"       . ?t)
+         ("/[Gmail].All Mail"    . ?a)))
+
+;; allow for updating mail using 'U' in the main view:
+(setq mu4e-get-mail-command "offlineimap")
+
+(require 'smtpmail)
+(setq message-send-mail-function 'smtpmail-send-it
+    smtpmail-stream-type 'starttls
+    smtpmail-default-smtp-server "smtp.gmail.com"
+    smtpmail-smtp-server "smtp.gmail.com"
+    smtpmail-smtp-service 587)
+
+;; don't keep message buffers around
+(setq message-kill-buffer-on-exit t)
--- rc/emacs-rc-user.el
+++
@@ -1,5 +0,0 @@
-(defun user-full-name ()
-  "Aliaksey Artamonau")
-
-(defun user-mail-address ()
-  "aliaksiej.artamonau@gmail.com")
