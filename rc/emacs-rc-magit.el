e3815e1ab7f613b1a636ec4a08c9f18c6af9a7c8
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-05-16 18:31:46 -0700

    Teach magit-view-file-history to reuse buffers.

--- rc/emacs-rc-magit.el
+++ rc/emacs-rc-magit.el
@@ -19,3 +19,12 @@
 
 ;; magit-view-file-history needs this
 (setq magit-sha1-abbrev-length (magit-abbrev-length))
+
+(defadvice magit-view-file-at-commit (around
+                                      magit-view-file-at-commit-reuse-buffer
+                                      activate)
+  (flet ((generate-new-buffer (name)
+                              (if (get-buffer name)
+                                  (kill-buffer name))
+                              (get-buffer-create name)))
+    ad-do-it))
