e08e5fa7c816c83ba16c5a3bf3c3c290aed12abd
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-07-18 21:35:42 -0700

    Better haskell repl.

--- rc/emacs-rc-haskell.el
+++ rc/emacs-rc-haskell.el
@@ -3,7 +3,6 @@
 (add-hook 'haskell-mode-hook 'global-hook-handler)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook #'(lambda () (require 'inf-haskell)))
 
 (autoload 'ghc-init "ghc" nil t)
 (add-hook 'haskell-mode-hook
@@ -12,3 +11,28 @@
             (setq ghc-ghc-options '("-Wall" "-fno-warn-name-shadowing"))
             (flymake-mode)))
 (add-hook 'haskell-mode-hook 'turn-on-hi2)
+
+;; Based upon https://github.com/paul7/dev-conf/blob/master/.emacs-haskell
+(defvar cabal-use-sandbox t)
+(setq haskell-process-type 'cabal-repl)
+(defun cabal-toggle-sandboxing-local ()
+  (interactive)
+  (set (make-local-variable 'cabal-use-sandbox) (not cabal-use-sandbox))
+  (message (format "This buffer haskell-process-type is ``%s''"
+                   (set (make-local-variable 'haskell-process-type)
+                        (if cabal-use-sandbox
+                            'cabal-repl
+                          'ghci)))))
+
+(defun cabal-toggle-sandboxing ()
+  (interactive)
+  (setq cabal-use-sandbox (not cabal-use-sandbox))
+  (message (format "haskell-process-type is ``%s''"
+                   (setq haskell-process-type
+                        (if cabal-use-sandbox
+                            'cabal-repl
+                          'ghci)))))
+
+(eval-after-load "haskell-mode"
+  '(progn (define-key haskell-mode-map [?\C-c ?\C-l] 'haskell-process-load-file)
+          (define-key haskell-mode-map [?\C-c ?\C-r] 'haskell-process-reload-file)))
