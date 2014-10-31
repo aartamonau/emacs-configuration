a3ed1b935ad0c6cd63aac98bb9a34ad6827cbe87
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-10-31 15:06:43 -0700

    accept go's love to tabs

--- rc/emacs-rc-go-mode.el
+++ rc/emacs-rc-go-mode.el
@@ -2,7 +2,12 @@
           (lambda () (c-subword-mode)))
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd "M-.") 'my/godef-jump)))
+(add-hook 'go-mode-hook 'my/dont-highlight-tabs)
 
 (defun my/godef-jump (point other-window)
   (interactive "d\nP")
   (godef-jump point other-window))
+
+(defun my/dont-highlight-tabs ()
+  (make-variable-buffer-local 'whitespace-style)
+  (setq 'whitespace-style (delq 'tabs whitespace-style)))
