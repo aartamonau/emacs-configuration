24ad87398d5d7b6964afb08c4632f3b5e23572b1
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-11-05 16:45:17 -0800

    show file details by default

--- rc/emacs-rc-dired.el
+++ rc/emacs-rc-dired.el
@@ -1,4 +1,5 @@
 (toggle-diredp-find-file-reuse-dir 1)
 
 (custom-set-variables
- '(dired-recursive-deletes 'always))
+ '(dired-recursive-deletes 'always)
+ '(diredp-hide-details-initially-flag nil))
