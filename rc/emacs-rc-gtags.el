4de7d61d92e3e996b6e6aafdb155e15906f1cdd5
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2015-01-24 22:41:02 -0800

    Make ggtags-navigation-mode less annoying.

--- rc/emacs-rc-gtags.el
+++ rc/emacs-rc-gtags.el
@@ -1,1 +1,8 @@
 (add-hook 'c-mode-common-hook 'ggtags-mode)
+
+(eval-after-load "ggtags"
+  '(progn (define-key ggtags-navigation-map "\M->" nil)
+          (define-key ggtags-navigation-map "\M-<" nil)
+          (define-key ggtags-navigation-map "\C-\M-s" nil)
+          (define-key ggtags-navigation-map "\C-c\M-<" 'fisrst-error)
+          (define-key ggtags-navigation-map "\C-c\M->" 'ggtags-navigation-last-error)))
