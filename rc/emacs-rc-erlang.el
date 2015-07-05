782be5a854f2bb0ea9a2be731c2b5120c31c547f
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2015-07-05 13:06:43 -0700

    Dirty and half-working edts setup for ns_server development.

--- rc/emacs-rc-erlang.el
+++ rc/emacs-rc-erlang.el
@@ -10,10 +10,49 @@
 (defconst erlang-emacs-dir
   (concat (file-name-as-directory erlang-tools-dir) "emacs"))
 
+(defun setup-edts ()
+  (require 'edts-start)
+  (require 'edts-project)
+
+  (require 'eproject)
+  (require 'f)
+
+  (defconst couchbase-root (f-expand "~/dev/membase/"))
+
+  (defun my/ns-server-project-selector (file-name)
+    (message "considering path %s" file-name)
+
+    (let ((res (and (f-ancestor-of? couchbase-root file-name)
+                    (f-traverse-upwards (lambda (path)
+                                          (equal (f-base path) "ns_server"))
+                                        file-name))))
+      (when res
+        ;; ugly but needed to let this project type overrule edts' and
+        ;; generic-git for the same root
+        (concat (f-expand res)
+                "/../"
+                (f-base res)))))
+
+  (define-project-type ns-server (edts)
+    (my/ns-server-project-selector file)
+    :xref-error-whitelist ("Call to undefined function 'ale_logger"))
+
+  (advice-add 'edts-code-eunit :around
+              (lambda (fun args)
+                "don't run unit tests in ns_server"
+                (unless (eq (eproject-type) 'ns-server)
+                  (apply fun args))))
+
+  ;; override edts' hackish defadvice
+  (defadvice eproject--all-types (around edts-eproject-types activate) ad-do-it))
+
 (when (file-accessible-directory-p erlang-emacs-dir)
   (add-to-list 'load-path erlang-emacs-dir)
 
   (require 'erlang-start)
+
+  (setup-edts)
+
   (add-hook 'erlang-mode-hook 'global-hook-handler)
   (add-hook 'erlang-mode-hook 'edts-mode)
   (add-hook 'edts-mode-hook
