8720ac9bf6f0b62668ffe9c1449717e308c6ad36
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2015-07-05 05:25:49 -0700

    Make el-get self-updatable.

--- rc/emacs-rc-el-get.el
+++ rc/emacs-rc-el-get.el
@@ -1,6 +1,13 @@
-(add-extension-load-path "el-get")
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-(require 'el-get)
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 
 (setq el-get-sources
       '((:name etags-select
