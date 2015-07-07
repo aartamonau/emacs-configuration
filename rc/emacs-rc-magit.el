7549dd893fc1b6d770a78b0cb6d6b0ecb9baee76
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2015-07-07 12:04:35 -0700

    Use vanilla magit-commit-amend.

--- rc/emacs-rc-magit.el
+++ rc/emacs-rc-magit.el
@@ -10,10 +10,6 @@
   (let ((name (file-relative-name file)))
     (vc-git-command buf 0 name "blame" "-w" rev)))
 
-(defun magit-commit-amend ()
-  (interactive)
-  (magit-commit t))
-
 (define-key magit-mode-map (kbd "C-c C-c") 'magit-commit)
 (define-key magit-mode-map (kbd "C-c C-a") 'magit-commit-amend)
 
