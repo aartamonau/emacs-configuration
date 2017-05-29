43aa614493c4f3cda013117a5f0ba395db4108a8
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2017-05-28 18:57:07 -0700

    avy

--- rc/emacs-rc-avy.el
+++ rc/emacs-rc-avy.el
@@ -3,8 +3,10 @@
 
 (setq aw-dispatch-always nil)
 
-(global-set-key (kbd "C-c j") 'avy-goto-subword-0)
+(global-set-key (kbd "C-c j") 'avy-goto-subword-1)
 (global-set-key (kbd "C-c l") 'avy-goto-line)
+(global-set-key (kbd "C-c C-j") 'avy-goto-subword-1)
+(global-set-key (kbd "C-c C-l") 'avy-goto-line)
 (global-set-key (kbd "C-x o") 'ace-window)
 
 (ace-link-setup-default)
