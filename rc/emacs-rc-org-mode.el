223971c31518b185e4a6546ed37b6faa8e148484
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2018-01-05 20:29:12 +0300

    [org] More room for the item in column view.

--- rc/emacs-rc-org-mode.el
+++ rc/emacs-rc-org-mode.el
@@ -149,7 +149,7 @@
      (search category-keep)))
 
  '(org-columns-default-format
-   "%30ITEM %SCHEDULED %TAGS %PRIORITY %8Effort(ESTIMATE){:} %8CLOCKSUM(CLOCK)")
+   "%75ITEM %SCHEDULED %TAGS %PRIORITY %8Effort(ESTIMATE){:} %8CLOCKSUM(CLOCK)")
 
  '(org-clock-in-switch-to-state "STARTED")
  '(org-clock-out-switch-to-state "PAUSED")
