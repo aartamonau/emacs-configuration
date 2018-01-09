541eb36e0b6e3435d6aaa1b694b1eebc62b8d6a8
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2018-01-09 03:41:55 +0300

    Un-"eval-after-load" sudo helpers.
    Because tramp doesn't get loaded by default. So they don't get defined
    either.

--- rc/emacs-rc-tramp.el
+++ rc/emacs-rc-tramp.el
@@ -1,24 +1,22 @@
-(eval-after-load "tramp"
-  '(progn
-     (defun sudo-file-name (filename)
-       (concat "/sudo::" filename))
+(defun sudo-file-name (filename)
+  (concat "/sudo::" filename))
 
-     (defun sudo-find-file (filename &optional wildcards)
-       "Calls find-file with filename with sudo-tramp-prefix prepended"
-       (interactive "fFind file with sudo ")
-       (let ((sudo-name (sudo-file-name filename)))
-         (apply 'find-file
-                (cons sudo-name (if (boundp 'wildcards) '(wildcards))))))
+(defun sudo-find-file (filename &optional wildcards)
+  "Calls find-file with filename with sudo-tramp-prefix prepended"
+  (interactive "fFind file with sudo ")
+  (let ((sudo-name (sudo-file-name filename)))
+    (apply 'find-file
+           (cons sudo-name (if (boundp 'wildcards) '(wildcards))))))
 
-     (defun sudo-reopen-file ()
-       "Reopen file as root by prefixing its name with
+(defun sudo-reopen-file ()
+  "Reopen file as root by prefixing its name with
 sudo-tramp-prefix and by clearing buffer-read-only"
-       (interactive)
-       (let ((file-name (expand-file-name (or buffer-file-name
-                                              list-buffers-directory)))
-             (buffer (current-buffer)))
-         (sudo-find-file file-name)
-         (kill-buffer buffer)))
+  (interactive)
+  (let ((file-name (expand-file-name (or buffer-file-name
+                                         list-buffers-directory)))
+        (buffer (current-buffer)))
+    (sudo-find-file file-name)
+    (kill-buffer buffer)))
 
-     (global-set-key (kbd "C-c o") 'sudo-find-file)
-     (global-set-key (kbd "C-c s") 'sudo-reopen-file)))
+(global-set-key (kbd "C-c o") 'sudo-find-file)
+(global-set-key (kbd "C-c s") 'sudo-reopen-file)
