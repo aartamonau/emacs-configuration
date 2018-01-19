e58a57646c0c3d2381685c2660490cbad4ff4f33
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2018-01-19 15:00:09 +0300

    Disable whitespace mode in dired-mode.

--- rc/emacs-rc-whitespace.el
+++ rc/emacs-rc-whitespace.el
@@ -1,5 +1,5 @@
 (global-whitespace-mode 1)
 
 (custom-set-variables
- '(whitespace-global-modes '(not org-mode))
+ '(whitespace-global-modes '(not org-mode dired-mode))
  '(whitespace-style '(face tabs trailing lines-tail empty tab-mark)))
