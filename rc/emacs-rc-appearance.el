ed87d2f4d472fedf58b1844827ee58a80e5d8311
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2018-04-29 13:12:31 -0700

    Increase font size.

--- rc/emacs-rc-appearance.el
+++ rc/emacs-rc-appearance.el
@@ -1,4 +1,4 @@
-(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-8.5"))
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-9.5"))
 (custom-set-faces
  '(variable-pitch ((t (:family "DejaVu Sans Mono")))))
 
