3bc52b34ccc76874d931996051509212deb5bd86
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2019-12-13 23:15:07 -0800

    Enable and configure winner-mode.

--- rc/emacs-rc-shackle.el
+++ rc/emacs-rc-shackle.el
@@ -1,4 +1,13 @@
+(require 'winner)
 (require 'shackle)
+
+(setq winner-dont-bind-my-keys t)
+(setq winner-ring-size 20)
+
+(define-key winner-mode-map (kbd "C-c [") 'winner-undo)
+(define-key winner-mode-map (kbd "C-c ]") 'winner-redo)
+
+(winner-mode)
 
 (setq shackle-rules
       '((compilation-mode :select nil :align below :size 0.3)
