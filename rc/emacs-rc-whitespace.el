4219dcc70da1a27b58f6335f1bdcb4d2d3db560c
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2020-01-10 17:47:00 -0800

    Disable whitespace mode in for magit.

--- rc/emacs-rc-whitespace.el
+++ rc/emacs-rc-whitespace.el
@@ -1,5 +1,5 @@
 (global-whitespace-mode 1)
 
 (custom-set-variables
- '(whitespace-global-modes '(not org-mode dired-mode))
+ '(whitespace-global-modes '(not org-mode dired-mode magit-status-mode))
  '(whitespace-style '(face tabs trailing lines-tail empty tab-mark)))
