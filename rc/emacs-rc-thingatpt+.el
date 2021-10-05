aff7051926fbab01c2eb6d603e57cb2e677e79a1
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2021-10-05 10:35:09 -0700

    Don't redefine alias thingatpt+ functions.
    Those screw with lsp-completion-at-point.

--- rc/emacs-rc-thingatpt+.el
+++ rc/emacs-rc-thingatpt+.el
@@ -1,6 +1,4 @@
 (eval-after-load "thingatpt"
   '(progn (require 'thingatpt+)
-          (tap-redefine-std-fns)
-
           (setq tap-near-point-y-distance 0)
           (setq tap-near-point-x-distance 10)))
