1251fe5124b5226f0658ca2901953ae93ef7e202
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2022-04-27 00:18:21 -0700

    Get rid of extension-management stuff.

--- init.el
+++ init.el
@@ -1,11 +1,12 @@
-(load "~/emacs/extensions-management.el")
-
 ;; custom custom-file :)
 (setq custom-file "~/emacs/custom.el")
 (load custom-file 'noerror)
 
 ;; no disabled commands for novice (does not clobber .emacs file)
 (setq disabled-command-function nil)
+
+(setq load-path
+      (append load-path '("~/emacs/rc")))
 
 ;; must be loaded after custom file
 (load "~/emacs/presets.el")
