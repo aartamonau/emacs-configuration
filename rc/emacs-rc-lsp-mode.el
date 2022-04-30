df622e61f2aa21a0d628f9b0be4d5d39e9c31930
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2022-04-30 16:03:41 -0700

    Disable lsp breadcrump, code actions, diagnostics.

--- rc/emacs-rc-lsp-mode.el
+++ rc/emacs-rc-lsp-mode.el
@@ -1,6 +1,9 @@
 (custom-set-variables
  '(lsp-keymap-prefix "C-.")
- '(lsp-restart 'ignore))
+ '(lsp-restart 'ignore)
+ '(lsp-headerline-breadcrumb-enable nil)
+ '(lsp-modeline-diagnostics-enable nil)
+ '(lsp-modeline-code-actions-enable nil))
 
 (require 'lsp-mode)
 
