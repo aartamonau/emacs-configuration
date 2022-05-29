56e0f00972c8e5cbd3403b022e35d1d4a407c9fd
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2022-05-29 12:19:56 -0700

    Stop overriding custom file.

--- init.el
+++ init.el
@@ -1,7 +1,3 @@
-;; custom custom-file :)
-(setq custom-file "~/emacs/custom.el")
-(load custom-file 'noerror)
-
 ;; no disabled commands for novice (does not clobber .emacs file)
 (setq disabled-command-function nil)
 
