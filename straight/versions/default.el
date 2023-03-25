fb336c987f4a2927eb007f9428839173239ea516
Aliaksei Artamonau <aliaksiej.artamonau@gmail.com>
2023-03-25 13:11:24 -0700

    Update dired-hacks

--- straight/versions/default.el
+++ straight/versions/default.el
@@ -6,7 +6,7 @@
  ("compat" . "d22ccfd8b1f39656da70d9c1ab63c015abd473e7")
  ("dash.el" . "76606f90774c5349f7adac20c33e6d37a1939a1f")
  ("diff-hl" . "d20f16bf5eadd66e775f215e800f25caddae8cb5")
- ("dired-hacks" . "7c0ef09d57a80068a11edc74c3568e5ead5cc15a")
+ ("dired-hacks" . "1596e516835099b96cb65d1dc372cfbdff6aea96")
  ("dired-plus" . "93998768b36944a1fa0bca27207372c2f8f8daca")
  ("doom-modeline" . "3300a10a19aa3817a0ae660d821398b8c6e562e9")
  ("el-get" . "a620c91fe7d6d482c0e7538df75e10af0af1bb16")
