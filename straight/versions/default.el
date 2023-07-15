f79f0e7e8f692c45b916f7928b58e7c3d1045688
Aliaksei Artamonau <aliaksiej.artamonau@gmail.com>
2023-07-15 16:29:34 -0700

    update ibuffer-vc

--- straight/versions/default.el
+++ straight/versions/default.el
@@ -34,7 +34,7 @@
  ("ht.el" . "1c49aad1c820c86f7ee35bf9fff8429502f60fef")
  ("hungry-delete" . "d919e555e5c13a2edf4570f3ceec84f0ade71657")
  ("hydra" . "317e1de33086637579a7aeb60f77ed0405bf359b")
- ("ibuffer-vc" . "9204001d1c5ca39409485c1574fd315b0e137a92")
+ ("ibuffer-vc" . "ac07ed3db55059bfa4b22e762ad9475dda37f1a1")
  ("ivy-xref" . "a82e8e117d2dd62c28b6a3e3d6e4cfb11c0bda38")
  ("let-alist" . "021fc10df2e44faba4728d849ee767cf890aa51a")
  ("line-comment-banner" . "5d76b07f713c3a8f8d513cab0e24fa77a91f912d")
