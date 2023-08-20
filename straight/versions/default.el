84b809d7ded2c9a114303254ad7cb89d35e0dac2
Aliaksei Artamonau <aliaksiej.artamonau@gmail.com>
2023-08-20 00:52:14 -0700

    Update ivy-hoogle

--- straight/versions/default.el
+++ straight/versions/default.el
@@ -35,7 +35,7 @@
  ("hungry-delete" . "d919e555e5c13a2edf4570f3ceec84f0ade71657")
  ("hydra" . "317e1de33086637579a7aeb60f77ed0405bf359b")
  ("ibuffer-vc" . "ac07ed3db55059bfa4b22e762ad9475dda37f1a1")
- ("ivy-hoogle" . "33a21802f8cfc2cba111e3bb525a8f50abba8c34")
+ ("ivy-hoogle" . "5f1ea089d0be34ea10a1b52d7b27df32e7e0f4c0")
  ("ivy-xref" . "a82e8e117d2dd62c28b6a3e3d6e4cfb11c0bda38")
  ("let-alist" . "021fc10df2e44faba4728d849ee767cf890aa51a")
  ("line-comment-banner" . "5d76b07f713c3a8f8d513cab0e24fa77a91f912d")
