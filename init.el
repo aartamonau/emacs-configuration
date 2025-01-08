a2dba5dc7e9b30ebc3b8527120ad23a2b0e9f18c
Aliaksei Artamonau <aliaksei.artamonau@crowdstrike.com>
2025-01-08 11:24:16 -0800

    Set fourmolu as the formatting provider

--- init.el
+++ init.el
@@ -1136,6 +1136,7 @@ of listed in `linum-mode-excludes'."
   (haskell-mode . (lambda ()
                     (setq-local standard-indent 2)
                     (setq-local lsp-rename-use-prepare nil))))
+  :custom (lsp-haskell-formatting-provider "fourmolu"))
 
 (use-package ivy-hoogle
   :bind (("C-. i" . ivy-hoogle))
